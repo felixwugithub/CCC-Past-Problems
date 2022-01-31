67d02e240d29b2daa1062e9e10cd6a62ea362d51
felixwugithub <94074190+felixwugithub@users.noreply.github.com>
2022-01-31 12:33:15 -0800

    Fixed input issue.
    Previously CCC online grader gave 0 marks, due to too many Scanner objects messing up the input format. Fixed by reducing the number of Scanner objects.

--- S1/src/Main.java
+++ S1/src/Main.java
@@ -1,36 +1,31 @@
 import java.util.Scanner;
+import java.io.*;
 
 public class Main {
 
     public static void main(String[] args){
-        Scanner N_in = new Scanner(System.in);
-        Scanner S_in = new Scanner(System.in);
-        Scanner H_in = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
+
 
         double totalArea = 0;
 
-        int N = N_in.nextInt();
+        int N = in.nextInt();
         int[] sides = new int[N+1];
         int[] heights = new int[N];
 
         for (int i = 0; i < sides.length; i++) {
-            sides[i] = S_in.nextInt();
+            sides[i] = in.nextInt();
         }
 
         for (int i = 0; i < heights.length; i++) {
-            heights[i] = H_in.nextInt();
+            heights[i] = in.nextInt();
         }
 
         for (int i = 0; i < N; i++) {
             totalArea += (double) ((sides[i]+sides[i+1]) * heights[i]) / 2;
         }
 
-        System.out.println(totalArea);
-
-
-
-
+        System.out.print(totalArea);
 
     }
-
 }
